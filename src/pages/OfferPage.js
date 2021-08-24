8770ff600d94705870c8ece7af3c8f2ed25ee39a
Karol Homziuk <karolhomziuk@gmail.com>
2021-08-25 00:22:21 +0200

    added new @media queries to styles

--- src/pages/OfferPage.js
+++ src/pages/OfferPage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/OfferPage.css";
 
 const OfferPage = () => {
   return (
     <div>
       <Link to="/offers">
-        <button>Powrót</button>
+        <button className="offer_return__btn">Powrót</button>
       </Link>
     </div>
   );
